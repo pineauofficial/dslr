1e115e3b5e283b6f42be2cff4b8acc14b6298455
Pierrick NEAU <pierrickn44@gmail.com>
2024-06-29 16:31:59 +0200

    no more imprecision on median, few on quartil

--- describe.py
+++ describe.py
@@ -83,14 +83,58 @@ def max(data, result):
                     max = data[column].iloc[i]
         max_result[column] = max
     result['max'] = max_result
-    
+ 
 def median(data, result):
     median_result = {}
-    
-# def median(line):	
+    for column in data.columns:
+        data[column] = pd.to_numeric(data[column], errors='coerce')
+        tmp_column = []
+        for i in range(len(data.index)):
+            if not math.isnan(data[column].iloc[i]):
+                tmp_column.append(data[column].iloc[i])
+        tmp_column.sort()
+        value = len(tmp_column) / 2
+        if value < len(tmp_column):
+            if value % 1 == 0:
+                median_result[column] = (tmp_column[int(value)] + tmp_column[int(value) - 1]) / 2
+            else:
+                median_result[column] = tmp_column[int(value)]    
+    result['median'] = median_result
 
-# def third_quartile(line):
 
+def first_quartile(data, result):
+    first_quartile_result = {}
+    for column in data.columns:
+        data[column] = pd.to_numeric(data[column], errors='coerce')
+        tmp_column = []
+        for i in range(len(data.index)):
+            if not math.isnan(data[column].iloc[i]):
+                tmp_column.append(data[column].iloc[i])
+        tmp_column.sort()
+        value = len(tmp_column) / 4
+        if value < len(tmp_column):
+            if value % 1 == 0:
+                first_quartile_result[column] = (tmp_column[int(value)] + tmp_column[int(value) - 1]) / 2
+            else:
+                first_quartile_result[column] = tmp_column[int(value)]
+    result['first_quartile'] = first_quartile_result
+
+def third_quartile(data, result):
+    third_quartile_result = {}
+    for column in data.columns:
+        data[column] = pd.to_numeric(data[column], errors='coerce')
+        tmp_column = []
+        for i in range(len(data.index)):
+            if not math.isnan(data[column].iloc[i]):
+                tmp_column.append(data[column].iloc[i])
+        tmp_column.sort()
+        value = len(tmp_column) / 4
+        if value < len(tmp_column):
+            if value % 1 == 0:
+                third_quartile_result[column] = (tmp_column[int(value)*3] + tmp_column[int(value)*3 - 1]) / 2
+            else:
+                third_quartile_result[column] = tmp_column[int(value)*3]
+    result['third_quartile'] = third_quartile_result
 
 result = pd.DataFrame()
 
@@ -98,8 +142,10 @@ count(data, result)
 mean(data, result)
 std(data, result)
 min(data, result)
-max(data, result)
+first_quartile(data, result)
 median(data, result)
+third_quartile(data, result)
+max(data, result)
 
 print(result.T)
 print(data.describe())
